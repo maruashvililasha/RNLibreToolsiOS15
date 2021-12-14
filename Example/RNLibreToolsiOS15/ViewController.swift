2084c03f3efe86399647cfbcefc3ecda728ef248
Lasha Maruashvili <38511844+maruashvililasha@users.noreply.github.com>
2021-12-14 13:03:28 +0400

    initial-Commit

--- Example/RNLibreToolsiOS15/ViewController.swift
+++ Example/RNLibreToolsiOS15/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by maruashvililasha@gmail.com on 12/14/2021.
 //  Copyright (c) 2021 maruashvililasha@gmail.com. All rights reserved.
 //
-
+import RNLibreToolsiOS15
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let s = RNLibreToolsiOS.shared
+        s.startSession { result in
+            switch result {
+            case .success(let gluecose):
+                DispatchQueue.main.async {
+                    print("\(gluecose)")
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
--- RNLibreToolsiOS15.podspec
+++ RNLibreToolsiOS15.podspec
@@ -28,10 +28,10 @@ TODO: Add long description of the pod here.
   s.source           = { :git => 'https://github.com/maruashvililasha@gmail.com/RNLibreToolsiOS15.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '9.0'
+  s.ios.deployment_target = '15.0'
 
   s.source_files = 'RNLibreToolsiOS15/Classes/**/*'
-  
+
   # s.resource_bundles = {
   #   'RNLibreToolsiOS15' => ['RNLibreToolsiOS15/Assets/*.png']
   # }
